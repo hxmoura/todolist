8ec5d551cfe7be150c9c1a41b9e48873b16df178
Henrique Moura <hxmoura@hotmail.com>
2023-04-04 18:22:16 -0300

    :rocket: Atualização de conexão

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     // baseURL: 'http://localhost:4000' // Backend - Local
-    baseURL: 'https://asd-rtd4.vercel.app' // Backend - Remote
+    baseURL: 'https://todolist-backend-hxmoura.vercel.app' // Backend - Remote
 })
 
 export default api
